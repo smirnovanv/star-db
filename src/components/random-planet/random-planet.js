f6b0f16740b47a9cbbc4f4da78b9c501dd2c2dc2
Nataly <smirnovanv89@gmail.com>
2022-04-05 19:17:35 +0300

    refactor: refactor planet api

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -8,11 +8,7 @@ class RandomPlanet extends Component {
     SwapiService = new SwapiService();
 
     state = {
-        id: null,
-        name: null,
-        population: null,
-        rotationPeriod: null,
-        diameter: null
+        planet: {}
     }
 
     constructor() {
@@ -20,23 +16,18 @@ class RandomPlanet extends Component {
         this.updatePlanet();
     }
 
+    onPlanetLoaded = (planet) => {
+        this.setState({planet});
+    }
+
     updatePlanet() {
         const id = Math.floor(Math.random()*10) + 2;
-        console.log(id);
         this.SwapiService.getPlanet(id)
-        .then((planet) => {
-          this.setState({
-              id,
-              name: planet.name,
-              population: planet.population,
-              rotationPeriod: planet.rotation_period,
-              diameter: planet.diameter
-          });
-        });
+        .then(this.onPlanetLoaded);
     }
 
     render () {
-        const {id, name, population, rotationPeriod, diameter} = this.state;
+        const {planet: {id, name, population, rotationPeriod, diameter}} = this.state;
 
         return (
             <div className="random-planet jumbotron rounded">
