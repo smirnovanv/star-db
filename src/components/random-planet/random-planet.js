a57b7b8b62a5277003b391b7747aa5009c88d22f
Nataly <smirnovanv89@gmail.com>
2022-04-06 14:53:00 +0300

    feat: add speaner

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,58 +1,83 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner';
 
 import './random-planet.css';
 
 class RandomPlanet extends Component {
 
-    SwapiService = new SwapiService();
+  SwapiService = new SwapiService();
 
-    state = {
-        planet: {}
-    }
+  state = {
+    loading: true,
+    planet: {}
+  }
 
-    constructor() {
-        super();
-        this.updatePlanet();
-    }
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
 
-    onPlanetLoaded = (planet) => {
-        this.setState({planet});
-    }
+  onPlanetLoaded = (planet) => {
+    this.setState({ planet,
+    loading: false });
+  }
 
-    updatePlanet() {
-        const id = Math.floor(Math.random()*10) + 2;
-        this.SwapiService.getPlanet(id)
-        .then(this.onPlanetLoaded);
-    }
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 10) + 2;
+    this.SwapiService.getPlanet(id)
+      .then(this.onPlanetLoaded);
+  }
 
-    render () {
-        const {planet: {id, name, population, rotationPeriod, diameter}} = this.state;
+  render() {
+    const {
+      planet,
+      loading
+    } = this.state;
 
-        return (
-            <div className="random-planet jumbotron rounded">
-              <img className="planet-image"
-                   src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
-              <div>
-                <h4>{name}</h4>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item">
-                    <span className="term">Population</span>
-                    <span>{population}</span>
-                  </li>
-                  <li className="list-group-item">
-                    <span className="term">Rotation Period</span>
-                    <span>{rotationPeriod}</span>
-                  </li>
-                  <li className="list-group-item">
-                    <span className="term">Diameter</span>
-                    <span>{diameter}</span>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            );
-    }
+    const spinner = loading ? <Spinner /> : null;
+    const content = !loading ? <PlanetView planet={planet} /> : null;
+
+    // if (loading) {
+    //   return <Spinner />
+    // }
+
+    return (
+      <div className="random-planet jumbotron rounded">
+       {spinner}
+       {content}
+      </div>
+    );
+  }
+};
+
+const PlanetView = ({ planet }) => {
+
+  const { id, name, population, rotationPeriod, diameter } = planet;
+
+  return (
+    <React.Fragment>
+      <img className="planet-image"
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+      <div>
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population</span>
+            <span>{population}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation Period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diameter</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  )
 };
 
 export default RandomPlanet;
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-spinner-0yy3tx41eqr"><div class="ldio-ovqjcy5hwho">
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div></div>
+    );
+}
+
+export default Spinner;
